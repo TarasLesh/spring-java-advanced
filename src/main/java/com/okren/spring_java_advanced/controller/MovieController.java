e35aad6d42be2ec61ce3a0b6669b1faeffb5c425
TarasLesh <t.leshchyshyn@gmail.com>
2020-06-26 17:58:12 +0300

    lesson 2 code (Spring REST p2 (PUT, DELETE))

--- src/main/java/com/okren/spring_java_advanced/controller/MovieController.java
+++ src/main/java/com/okren/spring_java_advanced/controller/MovieController.java
@@ -2,15 +2,14 @@ package com.okren.spring_java_advanced.controller;
 
 import com.okren.spring_java_advanced.model.Movie;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
- // RestController (на відміну від Controller) бере дані, які ми повертаємо, та перетвоює в JSON
- //          і автоматично вішає анотацію @ResponseBody
+// RestController (на відміну від Controller) бере дані, які ми повертаємо, та перетвоює в JSON
+//          і автоматично вішає анотацію @ResponseBody
 @RestController
 @RequestMapping(value = "/movies")
 public class MovieController {
@@ -38,5 +37,39 @@ public class MovieController {
         movies.add(movie);
 //        return ResponseEntity.accepted().body(movie);
         return movie;
+    }
+
+    //                       movies/id
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)   // в PATCH можна надсилати лише одне поле
+    public Movie updateMovie(@RequestBody Movie movie, @PathVariable int id) {
+        Optional<Movie> movieOptional = movies.stream()
+                .filter(movie1 -> movie1.getId() == id)
+                .findFirst();
+
+        if (movieOptional.isPresent()) {
+            Movie movieFromList = movieOptional.get();
+            movies.set(movies.indexOf(movieFromList), movie);
+
+        } else {
+            movie.setId(id);
+            movies.add(movie);
+        }
+//        if (movieFromList == null){
+//            movies.add(movie);
+//        } else {
+//            movies.set(id, movie);
+//        }
+        return movie;
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteMovie(@PathVariable Integer id, @RequestParam(value = "test") String test1){  // RequestParam ..url?test=some_param
+
+        System.out.println(test1);
+        Optional<Movie> movieOptional = movies.stream()
+                .filter(movie1 -> movie1.getId() == id)
+                .findFirst();
+        movieOptional.ifPresent(movie -> movies.remove(movie));
     }
 }
